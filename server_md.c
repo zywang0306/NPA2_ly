03fd8a72801d931bab1a66326b157e3f7b2f76b9
zywang0306 <43312614+zywang0306@users.noreply.github.com>
2018-10-08 16:47:07 -0500

    Update server_md.c

--- server_md.c
+++ server_md.c
@@ -19,7 +19,58 @@ int user_number = 0;
 int flag = 0;
 int Max_Client;
 struct SBCP_Client *client;
+struct sockaddr_in6 my_addr, their_addr; // IPv6
 
+// IPv6
+void ipv6connect(int myport, int lisnum, struct sockaddr_in6 my_addr ) {
+	socklen_t len;
+	int sockfd, new_fd;
+	char buf[1024];
+	struct sockaddr_in6 their_addr;
+
+
+	if ((sockfd = socket(PF_INET6, SOCK_STREAM, 0)) == -1) { // IPv6
+		perror("socket");
+		exit(1);
+	}
+	else
+		printf("socket created/n");
+
+	bzero(&my_addr, sizeof(my_addr));
+
+	my_addr.sin6_family = PF_INET6;    // IPv6
+	my_addr.sin6_port = htons(myport);   // IPv6
+		my_addr.sin6_addr = in6addr_any;            // IPv6
+
+	if (bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr_in6))  // IPv6
+		== -1) {
+		perror("bind");
+		exit(1);
+	}
+	else
+		printf("binded/n");
+
+	if (listen(sockfd, lisnum) == -1) {
+		perror("listen");
+		exit(1);
+	}
+	else
+		printf("begin listen/n");
+
+	while (1) {
+		len = sizeof(struct sockaddr);
+		if ((new_fd =
+			accept(sockfd, (struct sockaddr *) &their_addr,
+				&len)) == -1) {
+			perror("accept");
+			exit(errno);
+		}
+		else
+			printf("server: got connection from %s, port %d, socket %d/n",
+				inet_ntop(AF_INET6, &their_addr.sin6_addr, buf, sizeof(buf)), // IPv6
+				their_addr.sin6_port, new_fd); // IPv6
+	}
+}
 void join_user(int sock_fd, char* user_name_, struct SBCP_Client *client){
     int curr_index = user_number - 1;
     client[curr_index].sock_fd = sock_fd;
